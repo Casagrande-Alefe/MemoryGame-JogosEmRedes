3dedc43ef7d88338639b0fb0ea22d80b61fa19ac
Cazagrande-Alefe <145375583+Casagrande-Alefe@users.noreply.github.com>
2025-06-23 14:31:15 -0300

    Trabalho Incompleto
    Vi até 1m35s do vídeo

---
+++ MemoryGames - Redes/Assets/Scripts/Card.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Card : MonoBehaviour
+{
+    [SerializeField] private Image iconImage;
+
+    public Sprite hiddenIconSprite;
+    public Sprite iconSprite;
+
+    public bool isSelected;
+
+}
